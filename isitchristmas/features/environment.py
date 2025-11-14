11846099f00cf2aae11f91943543407aaf732a2d
Saul Aguiar <saulaguiar@Turbosflamas.local>
2025-11-14 09:48:29 -0700

    more changes

--- isitchristmas/features/environment.py
+++ isitchristmas/features/environment.py
@@ -2,9 +2,20 @@ import behave_webdriver
 from behave_webdriver.steps import *
 
 def before_all(context):
-    context.behave_driver = behave_webdriver.Chrome(
-        headless=True
-    )
+    chrome_opts = {
+        "chromeOptions": {
+            "args": [
+                "--headless",            # required for GitHub Actions
+                "--no-sandbox",
+                "--disable-gpu",
+                "--disable-dev-shm-usage",
+                "--disable-extensions",
+                "--window-size=1920,1080"
+            ]
+        }
+    }
+
+    context.behave_driver = behave_webdriver.Chrome(options=chrome_opts)
 
 def after_all(context):
     if hasattr(context, "behave_driver"):
