ef93de6683c5a5e60f1d98f705a8c8e38df14bc5
Saul Aguiar <saulaguiar@Turbosflamas.local>
2025-11-14 09:45:17 -0700

    more changes

--- isitchristmas/features/environment.py
+++ isitchristmas/features/environment.py
@@ -2,20 +2,9 @@ import behave_webdriver
 from behave_webdriver.steps import *
 
 def before_all(context):
-    chrome_opts = {
-        "chromeOptions": {
-            "args": [
-                "--headless",            
-                "--no-sandbox",
-                "--disable-gpu",
-                "--disable-dev-shm-usage",
-                "--disable-extensions",
-                "--window-size=1920,1080"
-            ]
-        }
-    }
-
-    context.behave_driver = behave_webdriver.Chrome(options=chrome_opts)
+    context.behave_driver = behave_webdriver.Chrome(
+        headless=True
+    )
 
 def after_all(context):
     if hasattr(context, "behave_driver"):
